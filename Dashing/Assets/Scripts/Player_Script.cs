e3f5c00714af32e92197bfe8b2c8f3ee2b3f1f27
DavidRMincer <33055698+DavidRMincer@users.noreply.github.com>
2018-10-23 16:30:29 +0100

    Core Mechanic

---
+++ Dashing/Assets/Scripts/Player_Script.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Script : MonoBehaviour
+{
+    public GameObject directionalArrow,
+        directionPivot;
+    public float dashForce,
+        rotationSpeed;
+
+    private Vector3 direction;
+    private Rigidbody rb;
+    private bool moving;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        moving = false;
+
+        //Dash();
+    }
+
+    private void FixedUpdate()
+    {
+        moving = !((Mathf.Abs(rb.velocity.x) < 0.001) &&
+            (Mathf.Abs(rb.velocity.y) < 0.001) &&
+            (Mathf.Abs(rb.velocity.z) < 0.001));
+
+        if (!moving) GetInput();
+    }
+
+    private void LateUpdate()
+    {
+        if (moving)
+        {
+            directionalArrow.SetActive(false);
+        }
+        else
+        {
+            directionalArrow.SetActive(true);
+        }
+    }
+
+    private void GetInput()
+    {
+        if (Input.GetButtonDown("Dash")) Dash();
+
+        else
+        {
+            transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
+        }
+
+    }
+
+    public void Dash()
+    {
+        direction = directionalArrow.transform.position - transform.position;
+        direction.y = 0.0f;
+
+        rb.AddForce(direction * dashForce);
+    }
+}
